8761889ea178a8c6dc927bfd5357752bc91bf6e1
gzwsjtu98 <gzwsjtu98@sjtu.edu.cn>
2026-03-25 21:48:55 +0800

    Drop missing teaser2 case imports

--- simulation/case_simulation/__init__.py
+++ simulation/case_simulation/__init__.py
@@ -3,6 +3,4 @@ from .lamp import Lamp
 from .sand_house import SandHouse
 from .persimmon import Persimmon
 from .tree import Tree
-from .teaser2_flour import Teaser2Flour
-from .teaser2_dough import Teaser2Dough
 from .two_duck import TwoDuck
